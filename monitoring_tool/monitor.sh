ec98ef342eea753069ffe9af5cd77ecac4e366c7
Narjes-Rezaei <n978666@gmail.com>
2026-06-04 15:14:54 +0900

    update

--- monitoring_tool/monitor.sh
+++ monitoring_tool/monitor.sh
@@ -7,33 +7,72 @@
 # ###########################
 #
 #
-echo "============================="
-echo "Linux Syetem Monitoring Tool "
-echo "============================="
-echo ""
+#
+GREEN="\e[32m"
+YELLOW="\e[33m"
+NC="\e[0m"
+#
+#
+#
 
-CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
-echo "CPU Usage: $CPU%"
-
-
-RAM=$(free | awk '/Mem:/ {printf("%.2f"), $3/$2 * 100}')
-echo "RAM Usage: $RAM%"
-
-
-#DISK=$(df -h / | awk 'NR==2 {print $5}')
-DISK=$(df -h / | grep -o '[0-9]\+%')
-echo "Disk Usage: $DISK"
+while true
+do
 
 
+  if [[ "$FIRST_RUN" = false ]]; then
+    printf "\033[15A"
+  fi
 
-echo ""
-UPTIME=$(uptime -p)
-echo "Uptime: $UPTIME"
+  CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
+  RAM=$(free | awk '/Mem:/ {printf("%.2f"), $3/$2 * 100}')
+  DISK=$(df -h / | grep -o '[0-9]\+%')
+
+  CPU_INT=${CPU%.*}
+  RAM_INT=${RAM%.*}
+
+  if [ "$CPU_INT" -gt 80 ]; then
+     CPU_STATUS="${RED}HIGH CPU USAGE! 🚨${NC}"
+  else
+     CPU_STATUS="${GREEN}NORMAL${NC}"
+  fi
+
+  if [ "$RAM_INT" -gt 80 ]; then
+     RAM_STATUS="${RED}HIGH RAM USAGE! 🚨${NC}"
+  else
+     RAM_STATUS="${GREEN}NORMAL${NC}"
+  fi
+
+  printf "\033[J"
+
+  echo "============================="
+  echo "Linux Syetem Monitoring Tool "
+  echo "============================="
+  printf "\n"
 
 
-USERS=$(who | wc -l)
-echo "Logged Users: $USERS"
+  echo -e "${GREEN}CPU Usage:${NC} $CPU%"
+  echo -e "${GREEN}RAM Usage:${NC} $RAM%"
+  echo -e "${GREEN}Disk Usage:${NC} $DISK"
+  printf "\n"
 
 
-TOP_PROCESS=$(ps -eo comm --sort=-%cpu | head -2 | tail -1)
-echo "Top Process: $TOP_PROCESS"
+  printf "\n"
+  echo -e "${GREEN}CPU Status:${NC} $CPU_STATUS"
+  echo -e "${GREEN}RAM Status:${NC} $RAM_STATUS"
+  printf "\n"
+
+  UPTIME=$(uptime -p)
+  echo -e "${GREEN}Uptime:${NC} $UPTIME"
+
+  USERS=$(who | wc -l)
+  echo -e "${GREEN}Logged Users:${NC} $USERS"
+
+  TOP_PROCESS=$(ps -eo comm,%cpu --sort=-%cpu | head -2 | tail -1)
+  echo -e "${GREEN}Top Process:${NC} $TOP_PROCESS"
+
+  FIRST_RUN=false
+
+  sleep 2
+done
+
+
