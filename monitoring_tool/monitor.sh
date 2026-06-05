ab5462c91179893eb90fe32a7e466d92861a1687
Narjes-Rezaei <n978666@gmail.com>
2026-06-06 02:28:51 +0900

    monitoring updated

--- monitoring_tool/monitor.sh
+++ monitoring_tool/monitor.sh
@@ -10,6 +10,7 @@
 #
 GREEN="\e[32m"
 YELLOW="\e[33m"
+WHITE="\e[37m"
 NC="\e[0m"
 #
 #
@@ -20,12 +21,14 @@ do
 
 
   if [[ "$FIRST_RUN" = false ]]; then
-    printf "\033[15A"
+    printf "\033[18A"
   fi
 
   CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
   RAM=$(free | awk '/Mem:/ {printf("%.2f"), $3/$2 * 100}')
   DISK=$(df -h / | grep -o '[0-9]\+%')
+  IP=$(hostname -I | awk '{print $1}')
+  HOST=$(hostname)
 
   CPU_INT=${CPU%.*}
   RAM_INT=${RAM%.*}
@@ -53,14 +56,25 @@ do
   echo -e "${GREEN}CPU Usage:${NC} $CPU%"
   echo -e "${GREEN}RAM Usage:${NC} $RAM%"
   echo -e "${GREEN}Disk Usage:${NC} $DISK"
-  printf "\n"
-
 
   printf "\n"
   echo -e "${GREEN}CPU Status:${NC} $CPU_STATUS"
   echo -e "${GREEN}RAM Status:${NC} $RAM_STATUS"
   printf "\n"
 
+############ Network ################
+  echo -e "${YELLOW}Hostname:${NC} $HOST"
+  echo -e "${YELLOW}IP Address:${NC} $IP"
+  if ping -c 1 8.8.8.8 &>/dev/null; 
+  then
+    INTERNET="${GREEN}Online${NC}"
+  else
+    INTERNET="${RED}Offline${NC}"
+  fi
+  echo -e "${GREEN}Internet:${NC} $INTERNET"
+  printf "\n"
+#####################################
+#
   UPTIME=$(uptime -p)
   echo -e "${GREEN}Uptime:${NC} $UPTIME"
 
